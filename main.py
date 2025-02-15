4c3af95455629f0aec82f598e54338c4ee956718
Denisque <denis2000.kuzik@gmail.com>
2025-02-15 19:03:12 +0300

    wip

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
+import sys
+from PyQt5.QtCore import QThread
+from PyQt5.QtWidgets import QApplication
+
 from utils.logger import logger
 from gui.main_window import MainWindow
-from sweeper.hackrf_sweeper import HackRFSweeper
-import matplotlib.pyplot as plt
-import numpy as np
-
-import cProfile
-import sys
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from PyQt5.QtCore import QThread, pyqtSignal, QObject
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QMessageBox
-
 
 if __name__ == '__main__':
     logger.info(f'Starting application in thread {int(QThread.currentThread().currentThreadId())}')
